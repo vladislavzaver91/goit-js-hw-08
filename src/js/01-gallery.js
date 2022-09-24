bf171c68a3856bf0105530a37797e6472ac613d7
Vladislav Zavertaylo <102727549+vladislavzaver91@users.noreply.github.com>
2022-09-24 12:50:08 +0300

    add solution to task 1, 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.addEventListener('click',() => {});
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </div>`;
+    }).join('');
+};
+
+    var lightbox = new SimpleLightbox('.gallery a', {captionDelay : 250, captionsData: 'alt'});
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,43 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+const onTimeUpdate = function (data) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+const savingData = localStorage.getItem(LOCAL_STORAGE_KEY);
+console.log(savingData);
+if (savingData) {
+    player.setCurrentTime(savingData)
+    .then(function() {})
+    .catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+            default:
+                break;
+        };
+    });
+};
+
+// function resumesPlayback() {
+//     const savingData = localStorage.getItem(LOCAL_STORAGE_KEY);
+//     if (savingData) {
+//     player.setCurrentTime(savingData)
+//     .then(function() {})
+//     .catch(function(error) {
+//         switch (error.name) {
+//             case 'RangeError':
+//                 break;
+//             default:
+//                 break;
+//         };
+//     });
+// };
+// };
