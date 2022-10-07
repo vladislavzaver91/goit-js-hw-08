9300652467bb09e9665a6092caff9a0e5b81635e
vladislavzaver91 <102727549+vladislavzaver91@users.noreply.github.com>
2022-10-07 15:51:37 +0000

    Deploying to gh-pages from @ vladislavzaver91/goit-js-hw-08@f404d35f9c268259275da6d42add7a9aaaa3e75e 🚀

--- 03-feedback.7c374883.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector("[name=message]")};e.form.addEventListener("submit",(function(e){e.preventDefault(),console.log("Poletela forma =>"),e.currentTarget.reset()})),e.textarea.addEventListener("input",(function(e){const t=e.currentTarget.value;localStorage.setItem("feedback-form-state",t),console.log(t)}));
-//# sourceMappingURL=03-feedback.7c374883.js.map
